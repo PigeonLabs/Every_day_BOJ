05319b64c2a9b3dbfd2eb5501a8b49490c4997d7
PigeonLabs <153859009+PigeonLabs@users.noreply.github.com>
2024-11-17 08:39:48 +0900

    [Bronze V] Title: Conveyor Belt Sushi, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/32326. Conveyor Belt Sushi/Conveyor Belt Sushi.cc
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+    int a,b,c;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    scanf("%d",&c);
+    printf("%d",a*3+b*4+c*5);
+    return 0;
+}
