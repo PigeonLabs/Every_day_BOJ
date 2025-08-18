bf6fb9aea0feafbccb3f0dec4de4cda583b1e67a
Seokju Jeong <153859009+PigeonLabs@users.noreply.github.com>
2025-08-18 23:00:38 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 1164 ms, Memory: 201220 KB -BaekjoonHub

---
+++ PyPy3/백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.py
@@ -0,0 +1,9 @@
+import sys,heapq
+input = sys.stdin.readline
+
+n = int(input())
+arr = []
+for _ in range(n):
+    heapq.heappush(arr,-int(input()))
+for _ in range(n):
+    print(-heapq.heappop(arr))
