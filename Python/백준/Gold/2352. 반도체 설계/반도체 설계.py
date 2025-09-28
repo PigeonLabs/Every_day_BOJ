11de78a515f4e7c4dee847d76b35792a7d461a3c
Seokju Jeong <153859009+PigeonLabs@users.noreply.github.com>
2025-09-28 15:04:41 +0900

    [Gold II] Title: 반도체 설계, Time: 60 ms, Memory: 37892 KB -BaekjoonHub

---
+++ Python/백준/Gold/2352. 반도체 설계/반도체 설계.py
@@ -0,0 +1,12 @@
+import sys,bisect
+input = sys.stdin.readline
+
+n = int(input())
+l = list(map(int, input().split()))
+d = []
+for i in range(n):
+    if not d or d[-1]<l[i]:
+        d.append(l[i])
+    else:
+        d[bisect.bisect_left(d,l[i])] = l[i]
+print(len(d))
