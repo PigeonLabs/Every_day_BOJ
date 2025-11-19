dc0fa4552212740d8d86227ea221961c10586a1e
Seokju Jeong <153859009+PigeonLabs@users.noreply.github.com>
2025-11-19 21:13:36 +0900

    [Bronze III] Title: 배수 찾기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/4504. 배수 찾기/배수 찾기.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+while True:
+    t = int(input())
+    if t == 0:
+        break
+    if t%n:
+        print(f"{t} is NOT a multiple of {n}.")
+    else:
+        print(f"{t} is a multiple of {n}.")
