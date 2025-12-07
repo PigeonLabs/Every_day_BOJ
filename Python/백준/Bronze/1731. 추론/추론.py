bd1653f1a93ea6bbe52b8f6f040366219995ee2d
Seokju Jeong <153859009+PigeonLabs@users.noreply.github.com>
2025-12-07 23:29:46 +0900

    [Bronze II] Title: 추론, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/1731. 추론/추론.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+if arr[1] - arr[0] == arr[2] - arr[1]:
+    print(arr[-1] + (arr[1] - arr[0]))
+else:
+    print(arr[-1] * (arr[1] // arr[0]))
