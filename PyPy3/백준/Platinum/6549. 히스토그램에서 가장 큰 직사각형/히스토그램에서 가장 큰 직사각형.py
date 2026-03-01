17364fc6640c32264e474ae0c7b12786260a0ad4
Seokju Jeong <153859009+PigeonLabs@users.noreply.github.com>
2026-03-01 18:01:01 +0900

    [Platinum V] Title: 히스토그램에서 가장 큰 직사각형, Time: 480 ms, Memory: 137924 KB -BaekjoonHub

---
+++ PyPy3/백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+
+def solve(s,e):
+    if s == e:
+        return t[s]
+    mid = (s+e)//2
+    l,r = mid,mid+1
+    minh = min(t[l],t[r])
+    ans = minh * 2
+    while l > s or r < e:
+        if l == s:
+            r += 1
+            minh = min(minh, t[r])
+        elif r == e:
+            l -= 1
+            minh = min(minh, t[l])
+        elif t[l - 1] < t[r + 1]:
+            r += 1
+            minh = min(minh, t[r])
+        else:
+            l -= 1
+            minh = min(minh, t[l])
+        ans = max(ans, minh * (r - l + 1))
+    return max(ans, solve(s,mid), solve(mid+1,e))
+
+while True:
+    n,*t = list(map(int,input().split()))
+    if not n:
+        break
+    print(solve(0,n-1))
