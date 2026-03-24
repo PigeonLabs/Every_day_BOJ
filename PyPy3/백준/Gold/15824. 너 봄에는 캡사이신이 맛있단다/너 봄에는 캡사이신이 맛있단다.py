145abac10b7575b41b278a6ed31523834531f64f
Seokju Jeong <153859009+PigeonLabs@users.noreply.github.com>
2026-03-24 11:08:11 +0900

    [Gold II] Title: 너 봄에는 캡사이신이 맛있단다, Time: 1008 ms, Memory: 109544 KB, Score: 50 point -BaekjoonHub

--- PyPy3/백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.py
+++ PyPy3/백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.py
@@ -6,6 +6,6 @@ arr = sorted(map(int, input().split()))
 res = 0
 for i in range(n):
     for j in range(i+1, n):
-        res += 2**(j-i-1)*(arr[j]-arr[i])%1000000007
+        res += pow(2, j-i-1, 1000000007) * (arr[j]-arr[i]) % 1000000007
     res %= 1000000007
 print(res)
