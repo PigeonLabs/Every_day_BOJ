ce00637e167770ef1ffc20a95458f5b727c21e03
Seokju Jeong <153859009+PigeonLabs@users.noreply.github.com>
2026-04-13 20:29:54 +0900

    [Silver IV] Title: 게시판, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Silver/3063. 게시판/게시판.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+for i in range(t):
+    x1, y1, x2, y2, x3, y3, x4, y4 = map(int, input().split())
+
+    w = min(x2, x4) - max(x1, x3)
+    h = min(y2, y4) - max(y1, y3)
+
+    print((x2 - x1) * (y2 - y1) - max(w, 0) * max(h, 0))
